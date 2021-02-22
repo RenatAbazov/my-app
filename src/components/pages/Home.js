6ee83395c2d8bdcd7053c04f125df46b265a4bf2
RenatAbazov <renat.abazov@gmail.com>
2021-02-21 21:54:52 -0600

    Components

---
+++ src/components/pages/Home.js
@@ -0,0 +1,33 @@
+<<<<<<< HEAD
+import React from 'react';
+import '../../App.css';
+import HeroSection from '../HeroSection';
+import reactDom from 'react-dom';
+import Cards from '../Cards';
+
+function Home() {
+    return (
+        <>
+            <HeroSection />
+            <Cards />
+        </>
+    )
+}
+
+export default Home;
+=======
+import '../../App.css';
+import HeroSection from "../HeroSection";
+import Cards from "../Cards";
+function Home () {
+  return (
+    <>
+      <HeroSection/>
+      <Cards />
+    </>
+
+  )
+}
+
+export default Home;
+>>>>>>> 12f0bd56dbaf258eac8ed260f89f3bf1f21596c8
